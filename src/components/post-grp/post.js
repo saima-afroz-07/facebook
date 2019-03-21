68deda39096c2aa9b67688b61a94e8bd20cdd8eb
saima-afroz-07 <47555574+saima-afroz-07@users.noreply.github.com>
2019-03-21 15:13:19 +0530

    Update post.js

--- src/components/post-grp/post.js
+++ src/components/post-grp/post.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Photos from './photos'
+import CreatePost from './create-post'
+ 
 
 export default class Post extends React.Component {
     render(){
         return (
             <div>
+            <CreatePost />
             <Photos />
         </div>
     
